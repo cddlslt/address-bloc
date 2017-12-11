c861367cbe60375c046dacf40725868f67a874d1
cddlslt <oatesmi.74@gmail.com>
2017-12-11 14:48:07 -0600

    checkpoint 4 complete

---
+++ spec/entry_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../models/entry'
+  RSpec.describe Entry do
+      describe "attributes" do
+        let(:entry) { Entry.new('Mike Oates', '512.201.9086', 'mike.oates.lfc@gmail.com') }
+
+        it "responds to name" do
+          expect(entry).to respond_to(:name)
+        end
+
+        it "reports its name" do
+          expect(entry.name).to eq('Mike Oates')
+        end
+
+        it "responds to phone number" do
+          expect(entry).to respond_to(:phone_number)
+        end
+
+        it "reports its phone_number" do
+          expect(entry.phone_number).to eq('512.201.9086')
+        end
+
+        it "responds to email" do
+          expect(entry).to respond_to(:email)
+        end
+
+        it "reports its email" do
+          expect(entry.email).to eq('mike.oates.lfc@gmail.com')
+        end
+      end
+
+      describe "#to_s" do
+        it "prints an entry as a string" do
+          entry = Entry.new('Mike Oates', '512.201.9086', 'mike.oates.lfc@gmail.com')
+          expected_string = "Name: Mike Oates\nPhone Number: 512.201.9086\nEmail: mike.oates.lfc@gmail.com"
+          expect(entry.to_s).to eq(expected_string)
+        end
+      end
+  end
