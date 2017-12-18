60de87340762a5654bcd085436a98d3c2e162c4b
cddlslt <oatesmi.74@gmail.com>
2017-12-18 13:26:39 -0600

    issues with test @checkpoint

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe AddressBook do
   let(:book) { AddressBook.new }
 
   def check_entry(entry, expected_name, expected_number, expected_email)
-    expect(entry_one.name).to eq expected_name
-    expect(entry_one.phone_number).to eq expected_number
-    expect(entry_one.email).to eq expected_email
+    expect(entry.name).to eq expected_name
+    expect(entry.phone_number).to eq expected_number
+    expect(entry.email).to eq expected_email
   end
 
   describe "attributes" do
@@ -72,26 +72,26 @@ RSpec.describe AddressBook do
 
     it "imports the 2nd entry" do
       book.import_from_csv("entries.csv")
-      entry_one = book.entries[1]
+      entry_two = book.entries[1]
       check_entry(entry_two, "Bob" "555-555-5415", "bob@blocmail.com")
     end
 
     it "imports the 3rd entry" do
       book.import_from_csv("entries.csv")
-      entry_one = book.entries[2]
+      entry_three = book.entries[2]
         check_entry(entry_three, "Joe" "555-555-3660", "joe@blocmail.com")
     end
 
     it "imports the 4th entry" do
       book.import_from_csv("entries.csv")
-      entry_one = book.entries[3]
+      entry_four = book.entries[3]
       check_entry(entry_four, "Sally" "555-555-4646", "sally@blocmail.com")
     end
 
     it "imports the 5th entry" do
       book.import_from_csv("entries.csv")
-      entry_one = book.entries[4]
-      check_entry(entry_one, "Sussie" "555-555-2036", "sussie@blocmail.com")
+      entry_five = book.entries[4]
+      check_entry(entry_five, "Sussie" "555-555-2036", "sussie@blocmail.com")
     end
   end
 
